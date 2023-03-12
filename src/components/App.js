c6165b5a15bf633ccabfca7b8db196b657594ed4
Abhishek singh <abhisheksiingh5261@gmail.com>
2023-03-12 15:27:36 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,45 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
 
-function ToDo({ todoId, createdAt, text }) {
+function ToDo({ todoId, createdAt }) {
   return (
     <tr>
-      <td>{todoId}</td>
-      <td><input type="text" value={text} /></td>
-      <td>{createdAt}</td>
+      <td>
+        <p>{todoId}</p>
+      </td>
+      <td>
+        <input placeholder="Enter your task" />
+      </td>
+      <td>
+        <p>{createdAt}</p>
+      </td>
     </tr>
   );
 }
 
-
-
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, createdAt: new Date().toISOString(), text: "Task 1" },
-    { id: 2, createdAt: new Date().toISOString(), text: "Task 2" },
-    { id: 3, createdAt: new Date().toISOString(), text: "Task 3" },
+    {
+      id: 'todo1',
+      createdAt: '20:30',
+    },
+    {
+      id: 'todo2',
+      createdAt: '18:00',
+    },
   ]);
 
   const reverseOrder = () => {
     setTodos([...todos].reverse());
-  }
+  };
 
   return (
     <div id="main">
       <button onClick={reverseOrder}>Reverse</button>
       <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>input</th>
-            <th>createdAt</th>
-          </tr>
-        </thead>
         <tbody>
-          {todos.map(todo => (
-            <ToDo key={todo.id} todoId={todo.id} createdAt={todo.createdAt} text={todo.text} />
+          {todos.map((todo) => (
+            <ToDo key={todo.id} todoId={todo.id} createdAt={todo.createdAt} />
           ))}
         </tbody>
       </table>
@@ -46,28 +48,3 @@ function App() {
 }
 
 export default App;
-
-
-// function App() {
-//   const [todos, setTodos] = useState([{
-//     id: 'todo1',
-//     createdAt: '20:30',
-//   }, {
-//     id: 'todo2',
-//     createdAt: '18:00',
-//   }
-//   ]);
-
-//   return (
-//     <div id="main">
-//       <button>Reverse</button>
-//       <table>
-//         <tbody>
-//         </tbody>
-//       </table>
-//     </div>
-//   )
-// }
-
-
-// export default App;
